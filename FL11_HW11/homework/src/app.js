8b542ce66f7116e3f5e4e7690ac73a1fef67ef1b
Volodymyr Bystrov <bystrovv@ukr.net>
2019-07-24 12:49:24 +0300

    Bug fixing

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,8 +1,8 @@
+const MaxItems = 10;
 let rootNode = document.getElementById('root');
 let input = document.getElementsByClassName('input-action')[0];
 let addButton = document.getElementsByClassName('icon-add')[0];
 let itemContainer = document.getElementsByClassName('items-container')[0];
-let dragItem; 
 itemContainer.addEventListener('dragover', doDragOver);
 itemContainer.addEventListener('drop', doDrop);
 input.addEventListener('input', toggleAddItem);
@@ -32,56 +32,27 @@ function createItem(inputText) {
   return item;
 }
 
-function drag(event){
-  event.dataTransfer.setData('text/html', event.target.outerHTML);
-  dragItem = event.target;
-  event.dataTransfer.effectAllowed = 'move';
-  event.dataTransfer.dropEffect = 'move';
-}
-function doDragOver(event){
-  event.preventDefault();
-}
-function doDrop(event){
-  event.preventDefault();
-  let data = event.dataTransfer.getData('text/html');
-  let insertItem = document.createElement('div');
-  let insertAfter;
-  for(let i=0; i<event.path.length;i++){
-    if(event.path[i].classList.contains('item')){
-      insertAfter = event.path[i];
-      break;
-    }
-  }
-  insertAfter.insertAdjacentElement('afterend', insertItem);
-  insertItem.outerHTML = data;
-  insertItem=insertAfter.nextSibling;
-  insertItem.draggable = true;
-  insertItem.addEventListener('dragstart', drag);
-  insertItem.getElementsByClassName('icon-checkbox-blank')[0].addEventListener('click', fillCheckbox);
-  insertItem.getElementsByClassName('icon-edit')[0].addEventListener('click', editItem);
-  insertItem.getElementsByClassName('icon-delete')[0].addEventListener('click', deleteItem);
-  itemContainer.removeChild(dragItem);
-}
-
 function addItem() {
   let newItem = createItem(input.value);
   newItem.draggable = true;
   newItem.addEventListener('dragstart', drag);
+  newItem.addEventListener('dragenter', doDragEnter);
+  newItem.addEventListener('dragleave', doDragLeave);
   itemContainer.appendChild(newItem);
-  if(itemContainer.childElementCount >= 10){
+  if (itemContainer.childElementCount >= MaxItems) {
     addButton.removeEventListener('click', addItem);
     addButton.classList.remove('enabled');
     addButton.classList.add('disabled');
-    let notif=document.getElementsByClassName('notification')[0];
+    let notif = document.getElementsByClassName('notification')[0];
     notif.style.visibility = 'visible';
     input.disabled = true;
   }
 }
 
-function editItem(event){
+function editItem(event) {
   let item = event.target.parentElement;
   let icons = item.getElementsByClassName('material-icons');
-  for(let i = 0; i < icons.length; i++){
+  for (let i = 0; i < icons.length; i++) {
     icons[i].classList.add('hide');
   }
   item.getElementsByClassName('item-text')[0].classList.add('hide');
@@ -97,7 +68,7 @@ function editItem(event){
   item.appendChild(iconSave);
 }
 
-function saveItem(event){
+function saveItem(event) {
   let item = event.target.parentElement;
   let inputText = item.getElementsByClassName('edit-text')[0];
   item.getElementsByClassName('item-text')[0].innerHTML = inputText.value;
@@ -105,15 +76,15 @@ function saveItem(event){
   item.removeChild(inputText);
   item.removeChild(iconSave);
   let hided = item.getElementsByClassName('hide');
-  while(hided.length>0){
+  while (hided.length > 0) {
     hided[0].classList.remove('hide');
   }
 }
 
-function deleteItem(event){
+function deleteItem(event) {
   let item = event.target.parentElement;
   itemContainer.removeChild(item);
-  if(itemContainer.childElementCount === 9){
+  if (itemContainer.childElementCount === MaxItems - 1) {
     addButton.addEventListener('click', addItem);
     addButton.classList.remove('disabled');
     addButton.classList.add('enabled');
@@ -142,3 +113,56 @@ function toggleAddItem() {
   }
 }
 
+function drag(event) {
+  let dragItem = event.target;
+  while (!dragItem.classList.contains('item')) {
+    dragItem = dragItem.parentElement;
+  }
+  let indexDragItem = 0;
+  for (let items = itemContainer.children; indexDragItem < items.length; indexDragItem++) {
+    if (items[indexDragItem].isSameNode(dragItem)) {
+      break;
+    }
+  }
+  indexDragItem = indexDragItem.toString();
+  event.dataTransfer.setData('text/plain', indexDragItem);
+  event.dataTransfer.effectAllowed = 'move';
+}
+
+function doDragEnter(event) {
+  let itemDragEnter = event.target;
+  while (!itemDragEnter.classList.contains('item')) {
+    itemDragEnter = itemDragEnter.parentElement;
+  }
+  let underline = document.createElement('div');
+  underline.classList.add('underline');
+  itemDragEnter.insertAdjacentElement('afterend', underline);
+}
+
+function doDragOver(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+}
+
+function doDragLeave(event) {
+  let itemDragLeave = event.target;
+  while (!itemDragLeave.classList.contains('item')) {
+    itemDragLeave = itemDragLeave.parentElement;
+  }
+  itemContainer.removeChild(itemDragLeave.nextElementSibling);
+}
+
+function doDrop(event) {
+  event.preventDefault();
+  let indexDragItem = event.dataTransfer.getData('text/plain');
+  indexDragItem = parseInt(indexDragItem);
+  let insertAfter;
+  for (let i = 0; i < event.path.length; i++) {
+    if (event.path[i].classList.contains('item')) {
+      insertAfter = event.path[i];
+      break;
+    }
+  }
+  itemContainer.removeChild(insertAfter.nextElementSibling);
+  insertAfter.insertAdjacentElement('afterend', itemContainer.children[indexDragItem]);
+}
